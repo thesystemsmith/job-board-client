87a8214b612912f4b8f7b85fe8a0aaf23fce716c
abdul rasheed <abdul.rasheed@simpleenergy.in>
2025-03-25 15:06:58 +0530

    fetching jobs data in home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,17 @@
+import { useEffect, useState } from 'react';
 import JobList from '../components/JobList';
-import { jobs } from '../lib/fake-data';
 import { getJobs } from '../lib/graphql/queries';
 
-getJobs().then((jobs) => console.log(jobs))
 
 function HomePage() {
+  const [jobs, setJobs] = useState([]);
+  useEffect(() => {
+    getJobs().then((jobs) => setJobs(jobs))
+  }, []);
+
+  console.log('homepage jobs:', jobs);
+  
+
   return (
     <div>
       <h1 className="title">
